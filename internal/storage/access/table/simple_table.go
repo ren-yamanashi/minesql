b570d3312b6ebfa1d95b1f9bd7deceebef9b2728
ren-yamanashi <pinokionigg@icloud.com>
2026-01-10 14:04:07 +0900

    impl

--- internal/storage/access/table/simple_table.go
+++ internal/storage/access/table/simple_table.go
@@ -2,6 +2,7 @@ package table
 
 import (
 	"minesql/internal/storage/access/btree"
+	"minesql/internal/storage/access/btree/node"
 	"minesql/internal/storage/bufferpool"
 	"minesql/internal/storage/disk"
 )
@@ -9,8 +10,9 @@ import (
 type SimpleTable struct {
 	// テーブルの内容が入っている B+Tree のメタページの ID
 	MetaPageId disk.PageId
-	// 左からいくつ目の列がプライマリキーなのかを表す数値
-	PrimaryKeyIndex int
+	// プライマリキーの列数 (プライマリキーは先頭から連続している想定)
+	// 例: プライマリキーが (id, name) の場合、PrimaryKeyCount は 2 になる
+	PrimaryKeyCount int
 }
 
 // 空のテーブルを新規作成する
@@ -23,12 +25,22 @@ func (t *SimpleTable) Create(bpm *bufferpool.BufferPoolManager) error {
 	return nil
 }
 
-
 // テーブルに行を挿入する
-// func (t *SimpleTable) Insert(bpm *bufferpool.BufferPoolManager, primaryKey []uint8, record [][]byte) error {
-// 	btree := btree.NewBTree(t.MetaPageId)
+// プライマリキーを key, 他のカラム値を value としたペアを作成し、B+Tree に挿入する
+func (t *SimpleTable) Insert(bpm *bufferpool.BufferPoolManager, primaryKey []uint8, record [][]byte) error {
+	btree := btree.NewBTree(t.MetaPageId)
 
-// 	// キーをエンコード
-// 	var encodedKey []byte
+	// キーをエンコード
+	var encodedKey []byte
+	Encode(record[:t.PrimaryKeyCount], &encodedKey)
 
-// }
+	// 値をエンコード
+	var encodedValue []byte
+	Encode(record[t.PrimaryKeyCount:], &encodedValue)
+
+	// B+Tree に挿入
+	return btree.Insert(bpm, node.Pair{
+		Key:   encodedKey,
+		Value: encodedValue,
+	})
+}
