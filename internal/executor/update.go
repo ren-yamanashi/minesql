7300e0e28b5c8c562598ccdbb42e671fd12c138d
ren-yamanashi <pinokionigg@icloud.com>
2026-03-14 14:18:41 +0900

    impl

---
+++ internal/executor/update.go
@@ -0,0 +1,74 @@
+package executor
+
+import "minesql/internal/storage"
+
+type SetColumn struct {
+	// 更新対象のカラムの位置 (インデックス)
+	Pos int
+	// 更新後の値
+	Value []byte
+}
+
+type Update struct {
+	tableName     string
+	SetColumns    []SetColumn
+	InnerExecutor Executor
+}
+
+func NewUpdate(tableName string, setColumns []SetColumn, innerExecutor Executor) *Update {
+	return &Update{
+		tableName:     tableName,
+		SetColumns:    setColumns,
+		InnerExecutor: innerExecutor,
+	}
+}
+
+func (upd *Update) Next() (Record, error) {
+	sm := storage.GetStorageManager()
+
+	tblMeta, err := sm.Catalog.GetTableMetadataByName(upd.tableName)
+	if err != nil {
+		return nil, err
+	}
+
+	tbl, err := tblMeta.GetTable()
+	if err != nil {
+		return nil, err
+	}
+
+	// 更新対象のレコードを先にすべて収集する
+	// (更新により Iterator が参照するページデータが破壊されるのを防ぐ)
+	var records []Record
+	for {
+		record, err := upd.InnerExecutor.Next()
+		if err != nil {
+			return nil, err
+		}
+		if record == nil {
+			break
+		}
+		records = append(records, record)
+	}
+
+	// 更新後のレコードを作成
+	var updatedRecords []Record
+	for _, record := range records {
+		updatedRecord := make(Record, len(record))
+		copy(updatedRecord, record)
+
+		for _, setCol := range upd.SetColumns {
+			updatedRecord[setCol.Pos] = setCol.Value
+		}
+		updatedRecords = append(updatedRecords, updatedRecord)
+	}
+
+	// 更新後のレコードで更新を実行
+	for i, record := range records {
+		err = tbl.Update(sm.BufferPoolManager, record, updatedRecords[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return nil, nil
+}
