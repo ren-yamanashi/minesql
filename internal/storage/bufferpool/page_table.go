80f30169cf91b88f9b7ecc5874a6741b7fea58be
ren-yamanashi <pinokionigg@icloud.com>
2026-03-15 12:24:13 +0900

    upd

--- internal/storage/bufferpool/page_table.go
+++ internal/storage/bufferpool/page_table.go
@@ -4,4 +4,11 @@ import (
 	"minesql/internal/storage/page"
 )
 
+// BufferId は、バッファプール内のバッファページを識別するための ID (index)
+type BufferId uint64
+
+// pageTable は、PageId と BufferId の対応関係を管理するテーブル
+// PageId に対応する BufferId を格納することで、該当のページがバッファプールのどの位置に格納されているかを特定できる
+// - key: PageId (ページID)
+// - value: BufferId (バッファID)
 type pageTable map[page.PageId]BufferId
