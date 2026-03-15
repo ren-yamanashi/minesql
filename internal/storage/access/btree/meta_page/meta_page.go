384b908d39553410024151bc30e1f12a0962ec19
ren-yamanashi <pinokionigg@icloud.com>
2026-03-16 00:02:18 +0900

    upd

--- internal/storage/access/btree/meta_page/meta_page.go
+++ internal/storage/access/btree/meta_page/meta_page.go
@@ -5,20 +5,20 @@ import (
 )
 
 type MetaPage struct {
-	// メタページ全体のバイト列
-	data []byte
+	data []byte // メタページ全体のバイト列 (先頭 8 byte はヘッダーで、ヘッダーにはルートページ ID が格納される)
 }
 
+// NewMetaPage は指定されたバイト列から MetaPage を生成する
 func NewMetaPage(data []byte) *MetaPage {
 	return &MetaPage{data: data}
 }
 
-// メタページのヘッダーからルートページ ID を読み取る
+// RootPageId はメタページのヘッダーからルートページ ID を読み取る
 func (mp *MetaPage) RootPageId() page.PageId {
 	return page.ReadPageIdFromPageData(mp.data, 0)
 }
 
-// メタページのヘッダーにルートページ ID を設定する
+// SetRootPageId はメタページのヘッダーにルートページ ID を設定する
 func (mp *MetaPage) SetRootPageId(rootPageId page.PageId) {
 	rootPageId.WriteTo(mp.data, 0)
 }
