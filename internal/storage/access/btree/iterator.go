a0b4ebd496eaf80c1d7144c898c7dbfaafb20a1b
ren-yamanashi <pinokionigg@icloud.com>
2026-03-17 23:41:40 +0900

    upd

--- internal/storage/access/btree/iterator.go
+++ internal/storage/access/btree/iterator.go
@@ -5,11 +5,19 @@ import (
 	"minesql/internal/storage/bufferpool"
 )
 
+// Iterator は B+Tree のリーフノード (双方向連結リスト) を走査する
+//
+// 全件スキャンや範囲検索などで、リーフノードを順番に走査するために使用する
 type Iterator struct {
-	bufferPage bufferpool.BufferPage
-	slotNum    int // 現在参照されているスロット番号 (slotted page のスロット番号)
+	bufferPage bufferpool.BufferPage // 現在参照しているバッファページ
+	slotNum    int                   // 現在参照されているスロット番号 (slotted page のスロット番号)
 }
 
+// newIterator は指定されたバッファページとスロット番号を持つイテレータを生成する
+//
+// bufferPage: イテレータが参照するバッファページ
+//
+// slotNum: イテレータが参照するスロット番号 (slotted page のスロット番号)
 func newIterator(bufferPage bufferpool.BufferPage, slotNum int) *Iterator {
 	return &Iterator{
 		bufferPage: bufferPage,
@@ -17,7 +25,7 @@ func newIterator(bufferPage bufferpool.BufferPage, slotNum int) *Iterator {
 	}
 }
 
-// 現在の key-value のペアを取得
+// Get は現在参照しているリーフノード (=バッファページ) の key-value のペアを取得
 func (iter *Iterator) Get() (node.Pair, bool) {
 	leafNode := node.NewLeafNode(iter.bufferPage.GetReadData())
 
@@ -33,7 +41,20 @@ func (iter *Iterator) Get() (node.Pair, bool) {
 	return node.NewPair(nil, nil), false
 }
 
-// 次の key-value ペアに進む
+// Next は次の key-value のペアを取得する
+func (iter *Iterator) Next(bp *bufferpool.BufferPool) (node.Pair, bool, error) {
+	pair, ok := iter.Get()
+	if !ok {
+		return node.NewPair(nil, nil), false, nil
+	}
+	err := iter.Advance(bp)
+	if err != nil {
+		return node.NewPair(nil, nil), false, err
+	}
+	return pair, true, nil
+}
+
+// Advance は次の key-value ペアに進む
 func (iter *Iterator) Advance(bp *bufferpool.BufferPool) error {
 	iter.slotNum++
 
@@ -67,17 +88,4 @@ func (iter *Iterator) Advance(bp *bufferpool.BufferPool) error {
 	iter.slotNum = 0
 
 	return nil
-}
-
-// 次の key-value のペアを取得する
-func (iter *Iterator) Next(bp *bufferpool.BufferPool) (node.Pair, bool, error) {
-	pair, ok := iter.Get()
-	if !ok {
-		return node.NewPair(nil, nil), false, nil
-	}
-	err := iter.Advance(bp)
-	if err != nil {
-		return node.NewPair(nil, nil), false, err
-	}
-	return pair, true, nil
 }
