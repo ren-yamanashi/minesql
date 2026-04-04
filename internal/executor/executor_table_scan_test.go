73f4cfbeb10873c13772057af8b7736e0e3994fe
ren-yamanashi <pinokionigg@icloud.com>
2026-04-04 21:35:35 +0900

    update

--- internal/executor/executor_table_scan_test.go
+++ internal/executor/executor_table_scan_test.go
@@ -141,15 +141,15 @@ func InitStorageEngineForTest(t *testing.T, dataDir string) *handler.Handler {
 	assert.NoError(t, err)
 
 	// 行を挿入
-	err = tbl.Insert(hdl.BufferPool, [][]byte{[]byte("a"), []byte("John"), []byte("Doe")})
+	err = tbl.Insert(hdl.BufferPool, 0, lock.NewManager(5000), [][]byte{[]byte("a"), []byte("John"), []byte("Doe")})
 	assert.NoError(t, err)
-	err = tbl.Insert(hdl.BufferPool, [][]byte{[]byte("b"), []byte("Alice"), []byte("Smith")})
+	err = tbl.Insert(hdl.BufferPool, 0, lock.NewManager(5000), [][]byte{[]byte("b"), []byte("Alice"), []byte("Smith")})
 	assert.NoError(t, err)
-	err = tbl.Insert(hdl.BufferPool, [][]byte{[]byte("c"), []byte("Bob"), []byte("Johnson")})
+	err = tbl.Insert(hdl.BufferPool, 0, lock.NewManager(5000), [][]byte{[]byte("c"), []byte("Bob"), []byte("Johnson")})
 	assert.NoError(t, err)
-	err = tbl.Insert(hdl.BufferPool, [][]byte{[]byte("d"), []byte("Eve"), []byte("Davis")})
+	err = tbl.Insert(hdl.BufferPool, 0, lock.NewManager(5000), [][]byte{[]byte("d"), []byte("Eve"), []byte("Davis")})
 	assert.NoError(t, err)
-	err = tbl.Insert(hdl.BufferPool, [][]byte{[]byte("e"), []byte("Charlie"), []byte("Brown")})
+	err = tbl.Insert(hdl.BufferPool, 0, lock.NewManager(5000), [][]byte{[]byte("e"), []byte("Charlie"), []byte("Brown")})
 	assert.NoError(t, err)
 
 	return hdl
