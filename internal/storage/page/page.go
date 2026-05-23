ecec0fefe2fcc82f066b6b4bea764fd5982c072b
ren-yamanashi <pinokionigg@icloud.com>
2026-05-23 13:55:56 +0900

    update

--- internal/storage/page/page.go
+++ internal/storage/page/page.go
@@ -1,7 +1,8 @@
 package page
 
 import (
-	"fmt"
+	"errors"
+	"strconv"
 )
 
 const (
@@ -9,12 +10,13 @@ const (
 	PageHeaderSize = 4
 )
 
-var ErrInvalidDataSize = fmt.Errorf("data size must be %d bytes", PageSize)
+var ErrInvalidDataSize = errors.New("data size must be " + strconv.Itoa(PageSize) + " bytes")
 
 // Page は 4KB のページ
 //   - ヘッダー: 先頭 4 バイト
 //   - ボディ: 残りバイト
 type Page struct {
+	data   []byte
 	Header []byte
 	Body   []byte
 }
@@ -24,14 +26,15 @@ func NewPage(data []byte) (*Page, error) {
 		return nil, err
 	}
 	return &Page{
+		data:   data,
 		Header: data[:PageHeaderSize], //nolint:gosec // CheckPageSize で len(data) == PageSize を検証済み
 		Body:   data[PageHeaderSize:], //nolint:gosec // CheckPageSize で len(data) == PageSize を検証済み
 	}, nil
 }
 
 // ToBytes はページ全体のバイト列を返す
-func (p Page) ToBytes() []byte {
-	return p.Header[:PageSize]
+func (p *Page) ToBytes() []byte {
+	return p.data
 }
 
 // CheckPageSize は data が 4KB であるかを確認する
