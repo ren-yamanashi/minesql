cb20b3fdf48cff5b3c74b5ac1cf41e7eb7c23748
ren-yamanashi <pinokionigg@icloud.com>
2026-05-24 17:32:23 +0900

    update

--- internal/storage/encode/memcomparable.go
+++ internal/storage/encode/memcomparable.go
@@ -1,36 +1,46 @@
 package encode
 
-import "slices"
+import (
+	"errors"
+	"slices"
+)
 
 const (
 	lengthInfoSize = 1
 	dataSize       = 8
 	blockSize      = dataSize + lengthInfoSize
+
+	// continuationMarker は後続ブロックが続くことを示す長さバイトの値
+	// blockSize = dataSize + lengthInfoSize なので、末尾ブロックの長さ 0..dataSize と区別できる
+	continuationMarker = byte(blockSize)
 )
 
+var ErrInvalidEncoding = errors.New("encode: invalid memcomparable encoding")
+
 // Encode は複数のバイト列を連結してエンコードする
+//   - dst: 既存バイト列の末尾に追記する場合に渡す (nil 可)
 //   - elements: エンコード対象のバイト列のスライス
-//   - dest: エンコードの結果の格納先のポインタ
-func Encode(elements [][]byte, dest *[]byte) {
+func Encode(dst []byte, elements [][]byte) []byte {
 	for _, element := range elements {
-		size := encodedSize(len(element))
-
-		*dest = slices.Grow(*dest, size)
-
-		encodeToMemcomparable(element, dest)
+		dst = slices.Grow(dst, encodedSize(len(element)))
+		dst = encodeToMemcomparable(dst, element)
 	}
+	return dst
 }
 
 // Decode はエンコードされたバイト列を複数のバイト列にデコードする
-//   - src: エンコードされたバイト列
-//   - elements: デコード結果の格納先のポインタ
-func Decode(src []byte, elements *[][]byte) {
+func Decode(src []byte) ([][]byte, error) {
+	var elements [][]byte
 	rest := src
 	for len(rest) > 0 {
-		element := []byte{}
-		decodeFromMemcomparable(&rest, &element)
-		*elements = append(*elements, element)
+		element, consumed, err := decodeFromMemcomparable(rest)
+		if err != nil {
+			return nil, err
+		}
+		elements = append(elements, element)
+		rest = rest[consumed:]
 	}
+	return elements, nil
 }
 
 // encodedSize はエンコード後のサイズを計測する
@@ -43,42 +53,49 @@ func encodedSize(size int) int {
 	return ((size + dataSize - 1) / dataSize) * blockSize
 }
 
-// バイト列を memcomparable 形式にエンコードする
-//   - src: エンコード対象のバイト列
-//   - dest: エンコード結果の格納先ポインタ
-func encodeToMemcomparable(src []byte, dest *[]byte) {
-	// 8 バイトずつブロックに分割して書き込む (長さ情報には 9 を付与)
+// encodeToMemcomparable は単一のバイト列を memcomparable 形式にエンコードし、dst の末尾に追記して返す
+func encodeToMemcomparable(dst, src []byte) []byte {
+	// 8 バイトずつブロックに分割して書き込む (長さ情報には continuationMarker を付与)
 	for len(src) > dataSize {
-		*dest = append(*dest, src[:dataSize]...)
+		dst = append(dst, src[:dataSize]...)
 		src = src[dataSize:]
-		*dest = append(*dest, byte(blockSize))
+		dst = append(dst, continuationMarker)
 	}
 
 	// 最後のブロックを書き込む (8 バイトに満たない場合はゼロ埋め)
 	copySize := len(src)
-	*dest = append(*dest, src...)
+	dst = append(dst, src...)
 	if pad := dataSize - copySize; pad > 0 {
-		*dest = append(*dest, make([]byte, pad)...)
+		dst = append(dst, make([]byte, pad)...)
 	}
-	*dest = append(*dest, byte(copySize))
+	dst = append(dst, byte(copySize))
+	return dst
 }
 
-// memcomparable 形式からバイト列をデコードする
-//   - src: デコード対象のバイト列のポインタ
-//   - dest: デコード結果の格納先のポインタ
-func decodeFromMemcomparable(src *[]byte, dest *[]byte) {
+// decodeFromMemcomparable は src の先頭から 1 要素分を memcomparable 形式からデコードし、デコード結果と消費したバイト数を返す
+func decodeFromMemcomparable(src []byte) ([]byte, int, error) {
+	dst := []byte{}
+	consumed := 0
 	for {
+		if len(src) < blockSize {
+			return nil, 0, ErrInvalidEncoding
+		}
+
 		// 長さ情報を取得
-		extra := (*src)[dataSize]
-		size := min(dataSize, int(extra))
+		lengthByte := src[dataSize]
+		if lengthByte > continuationMarker {
+			return nil, 0, ErrInvalidEncoding
+		}
 
 		// データをコピー
-		*dest = append(*dest, (*src)[0:size]...)
-		*src = (*src)[blockSize:]
+		size := min(dataSize, int(lengthByte))
+		dst = append(dst, src[:size]...)
+		src = src[blockSize:]
+		consumed += blockSize
 
-		// 長さ情報が 9 未満の場合、最後のブロックなので終了
-		if extra < byte(blockSize) {
-			break
+		// 長さ情報が continuationMarker 未満の場合、最後のブロックなので終了
+		if lengthByte < continuationMarker {
+			return dst, consumed, nil
 		}
 	}
 }
